00c83045586dde5a58c5aecb0d3888c75fd8f9a3
Allan Bowe <allnbwe@gmail.com>
2021-05-19 22:26:43 +0300

    chore: automated commit

--- src/styledComponents/blog.js
+++ src/styledComponents/blog.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const StyledHeading = styled.h5`
-  font-family: 'Montserrat', 'HelveticaNeue', 'Helvetica Neue', Helvetica, Arial,
+  font-family: 'HelveticaNeue', 'Helvetica Neue', Helvetica, Arial,'Montserrat'
     sans-serif;
   text-transform: uppercase;
   letter-spacing: 1px;
